998e5a646c508736f38bacf9945359d9b1f5be04
jillbowen <jillian.b.short@gmail.com>
2021-02-16 17:40:53 -0800

    get and render plants

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const endPoint = "http://localhost:3000/api/v1/plants"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getPlants()
+})
+
+function getPlants() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(plants => {
+        renderPlants(plants)
+    })
+}
+
+function renderPlants(plants) {
+    plants.data.forEach(plant => {
+        const plantInfo = `
+            <div data-id=${plant.id}>
+                <h3>${plant.attributes.name}</h3>
+                <p>${plant.attributes.description}</p>
+                <img src=${plant.attributes.image_url} height="250" width="300">
+                <p>Find this in the ${plant.attributes.garden.name} garden.</p>
+                <button data-id=${plant.id}>Edit</button>
+            <div>
+            <br>`;
+
+            document.querySelector('#plant-container').innerHTML += plantInfo
+    });
+}
