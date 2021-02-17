4cfbc833e7070727db45ca141ad53cfb8d9af606
jillbowen <jillian.b.short@gmail.com>
2021-02-16 19:43:13 -0800

    add create plant form and functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// JS Mantra * When some event happens, 
+// I want to make what kind of fetch, 
+// and then manipulate the DOM in what way?*
+
 const endPoint = "http://localhost:3000/api/v1/plants"
 
 document.addEventListener('DOMContentLoaded', () => {
     getPlants()
-})
+
+    const createPlantForm = document.querySelector("#create-plant-form")
+
+    createPlantForm.addEventListener("submit", (e) => createPlantFormHandler(e))
+});
 
 function getPlants() {
     fetch(endPoint)
@@ -10,7 +18,7 @@ function getPlants() {
     .then(plants => {
         renderPlants(plants)
     })
-}
+};
 
 function renderPlants(plants) {
     plants.data.forEach(plant => {
@@ -26,4 +34,30 @@ function renderPlants(plants) {
 
             document.querySelector('#plant-container').innerHTML += plantInfo
     });
+};
+
+function createPlantFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector("#plant-name").value 
+    const descriptionInput = document.querySelector("#plant-description").value
+    const imgInput = document.querySelector("#plant-img-url").value
+    const gardenId = parseInt(document.querySelector("#gardens").value)
+    postFetch(nameInput, descriptionInput, imgInput, gardenId)
+};
+
+function postFetch(name, description, img_url, garden_id) {
+    fetch(endPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify( {
+            name: name,
+            description: description,
+            img_url: img_url,
+            garden_id: garden_id
+        })
+    })
+    .then(response => response.json())
+    .then(plant => {
+        console.log(plant);
+    })
 }
