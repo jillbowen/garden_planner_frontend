628e49be37fe51e15044419c3c84ba3c6600fc64
jillbowen <jillian.b.short@gmail.com>
2021-02-18 20:30:29 -0800

    impletmented oojs refactor

---
+++ src/plant.js
@@ -0,0 +1,24 @@
+class Plant {
+
+    constructor(plant, plantAttributes) {
+        this.id = plant.id
+        this.name = plantAttributes.name
+        this.description = plantAttributes.description
+        this.image_url = plantAttributes.image_url
+        this.garden = plantAttributes.garden
+        Plant.all.push(this)
+    }
+
+    renderPlantCard() {
+        return`
+            <div data-id=${this.id}>
+                <h3>${this.name}</h3>
+                <p>${this.description}</p>
+                <img src=${this.image_url} height="250" width="300">
+                <p>Find this in the ${this.garden.name} garden.</p>
+                <button data-id=${this.id}>Edit</button>
+            <div>
+            <br>`;
+    }
+}
+Plant.all = [];
